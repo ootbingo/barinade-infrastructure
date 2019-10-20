4bdc21e78b9aa961c9b838c0e4ed36a07cbbc2ff
scaramangado <scaramangado@gmail.com>
2019-10-20 16:14:31 +0200

    Disable Let's encrypt staging

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(scaramangado.de www.scaramangado.de)
 rsa_key_size=4096
 data_path="/docker/volumes/certbot"
 email="scaramangado@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
