811ed32e6c57e61892f64b7942a5290afb65fde4
scaramangado <scaramangado@gmail.com>
2021-03-07 14:28:25 +0100

    Fix deplyoment script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,5 @@
 
 echo "$2" | docker login docker.pkg.github.com -u $1 --password-stdin
 
-case $(git branch --show-current) in
-	master) ENV=prod ;;
-	develop) ENV=int ;;
-	feature/gh-actions) ENV=int ;;
-	*) echo "Building images is only available on master and develop" && exit 1 ;;
-esac
-
 docker-compose -f /barinade/barinade-infrastructure/$ENV/docker-compose.yml up -d --force-restart bot
 
