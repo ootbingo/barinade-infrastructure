babe142485ba94dbf449cf5c5c08d7c59df7f090
scaramangado <scaramangado@gmail.com>
2021-03-07 17:35:59 +0100

    Pull before restarting container

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 echo "$2" | docker login docker.pkg.github.com -u $1 --password-stdin
 
-docker-compose -f /barinade/barinade-infrastructure/$ENV/docker-compose.yml up -d --force-restart bot
+docker-compose pull bot
+docker-compose -f /barinade/barinade-infrastructure/$ENV/docker-compose.yml up -d --force-recreate bot
 
