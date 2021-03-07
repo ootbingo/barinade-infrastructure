bd4ad4b97469aa732449da4a2d9e3afec1d406b6
scaramangado <scaramangado@gmail.com>
2021-03-07 17:40:16 +0100

    Update prod definition

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 
 echo "$2" | docker login docker.pkg.github.com -u $1 --password-stdin
 
+case $3 in
+	master) ENV=prod ;;
+	develop) ENV=int ;;
+	feature/gh-actions) ENV=int ;;
+	*) echo "Branch not supported" && exit 1 ;;
+esac
+
 docker-compose pull bot
 docker-compose -f /barinade/barinade-infrastructure/$ENV/docker-compose.yml up -d --force-recreate bot
 
